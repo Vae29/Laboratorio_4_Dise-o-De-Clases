096b99476755ef9a334726c63e3e9955043f77a5
Vae29 <valesalvalen@gmail.com>
2024-10-08 21:55:54 -0500

    Add files via upload

---
+++ Ejercicio_1.py
@@ -0,0 +1,14 @@
+class Persona:
+    def _init_(self,nombre,edad):
+        self.nombre= nombre
+        self.edad= edad
+        self.estado= "despierto"
+
+    def mimir(self):
+        self.estado= "durmiendo"
+
+    def wakeup(self):
+        self.estado="despierto"
+
+    def mostrar_estado(self):
+        print(f"{self.nombre} tiene {self.edad} años y esta {self.estado}")
